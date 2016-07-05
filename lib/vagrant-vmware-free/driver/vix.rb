81c40157190bdae4e0f036d58f780fc7fc9d86e5
Joe Gorse <jhgorse@gmail.com>
2016-07-05 14:45:25 -0400

    Let's be a little more clever about the VIX lib path.

--- lib/vagrant-vmware-free/driver/vix.rb
+++ lib/vagrant-vmware-free/driver/vix.rb
@@ -3,12 +3,19 @@ require 'ffi'
 module VagrantPlugins
   module ProviderVMwareFree
     module Driver
-      
+
       module VIX
         class VIXError < StandardError; end
         extend FFI::Library
 
-        ffi_lib '/Applications/VMware Fusion.app/Contents/Public/libvixAllProducts.dylib'
+        if /darwin/ =~ RUBY_PLATFORM
+                ffi_lib '/Applications/VMware Fusion.app/Contents/Public/libvixAllProducts.dylib'
+        elsif /cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM
+                ffi_lib 'c:/Program Files (x86)/VMware/VMware VIX/VixAllProductsDyn.dll'
+        else
+                ffi_lib '/usr/lib/vmware-vix/libvixAllProducts.so'
+        end
+
       end
     end
   end
